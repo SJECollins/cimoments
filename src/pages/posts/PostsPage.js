4bae89cdee9fb55442cee09adf83343b5422e4d4
SJECollins <66878688+SJECollins@users.noreply.github.com>
2023-04-11 12:01:27 +0000

    Add popular profiles

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,7 +16,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
-// import PopularProfiles from "../profiles/PopularProfiles";
+import PopularProfiles from "../profiles/PopularProfiles";
 import { useCurrentUser } from "../../contexts/currentUserContexts";
 
 function PostsPage({ message, filter = "" }) {
@@ -52,7 +52,7 @@ function PostsPage({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        {/* <PopularProfiles mobile /> */}
+        <PopularProfiles mobile />
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={styles.SearchBar}
@@ -92,7 +92,7 @@ function PostsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        {/* <PopularProfiles /> */}
+        <PopularProfiles />
       </Col>
     </Row>
   );
