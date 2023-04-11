e40f74bf31571c8295a70adf8c0e45e781c3a4d0
SJECollins <66878688+SJECollins@users.noreply.github.com>
2023-04-11 14:39:49 +0000

    Add profile page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostEditForm from './pages/posts/PostEditForm'
 import PostPage from './pages/posts/PostPage'
 import PostsPage from './pages/posts/PostsPage'
 import { useCurrentUser } from './contexts/currentUserContexts'
+import ProfilePage from './pages/profiles/ProfilePage'
 
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
                     <Route exact path="/posts/create" render={() => <PostCreateForm />} />
                     <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
                     <Route exact path="/posts/:id" render={() => <PostPage />} /> 
+                    <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
                     <Route render={() => <h1>Page not found!</h1>} />
                 </ Switch>
             </Container>
